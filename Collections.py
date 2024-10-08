1c84c49520cc28b9cad7d1999f326e80ba7b1671
Lazar Knezevic <141447582+Lazzzer00@users.noreply.github.com>
2024-10-08 17:51:09 +0200

    Update Collections.py

--- Collections.py
+++ Collections.py
@@ -1,6 +1,4 @@
-# these are type-assertion collection types
-
-def dictionaries():
+def DICT():
   person = {"name": "Lazzzer", "age": 15, "city": "New York"}
   person = dict(name="Lazzzer", age=15, city="New York") # these are the ways to difine the same dictionary
   person = dict([("name", "Lazzzer"), ("age", 15), ("city", "New York")])
@@ -9,30 +7,32 @@ def dictionaries():
   assert person["age"] == 15
   assert person["city"] == "New York"
 
-def lists(): # lists and arrays are the same, but lists can be heterogeneous, while arrays are homogeneous
+def LIST(): # lists and arrays are the same, but lists can be heterogeneous, while arrays are homogeneous
   fruitSet = {"banana", "apple", "orange"};
   fruits = list(fruitSet);
   assert fruits[0] in fruitSet
   assert len(fruits) == 3;
+  assert list("String") == ["S", "t", "r", "i", "n", "g"]
 
-def tuples(): # the same as a list, but cannot be changed
+def TUPLE(): # the same as a list, but cannot be changed
   coordinates = tuple([3, 4])
   coordinates = (3, 4)
   assert coordinates[0] == 3
   assert coordinates[1] == 4
   # coordinates[0] = 5 !ERROR!
 
-def sets(): # sets cannot contain duplicates
+def SET(): # sets cannot contain duplicates
   fruitList = ["apple", "cherry", "apple", "banana"]
   fruits = set(fruitList)
   fruits = {"apple", "cherry", "banana"}
   assert len(fruits) == 3
   assert "banana" in fruits
 
-def frozensets(): # the same as a set, but cannot be changed
+def FROZENSET(): # the same as a set, but cannot be changed
   colorList = ["red", "blue", "green", "green"]
   colors = frozenset(colorList)
   assert len(colors) == 3
   assert "red" and "blue" in colors
   # colors.add("yellow") !ERROR!
+
 
